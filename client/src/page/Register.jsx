8d9e749eb4cc3b6cfd955c33e928de26183da3dc
RookieZxy <46013671+RookieZxy@users.noreply.github.com>
2022-12-04 21:19:40 -0500

    setupProxy

---
+++ client/src/page/Register.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import axios from "axios";
+
+const Register = () => {
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const account = e.target[0].value;
+      const displayName = e.target[1].value;
+      const password = e.target[2].value;
+      const email = e.target[3].value;
+      const file = e.target[4].value;
+      const user = {
+        account: e.target[0].value,
+        displayName: e.target[1].value,
+        password: e.target[2].value,
+        email: e.target[3].value,
+        file: e.target[4].value,
+        isAdmin: false,
+      };
+      console.log(user);
+      const url = `http://localhost:4000/users/signup`;
+      await axios.post(url, user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">zwitter</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input required type="text" placeholder="account" />
+          <input required type="text" placeholder="display name" />
+          <input required type="password" placeholder="password" />
+          <input required type="email" placeholder="email" />
+          <input required  type="file" id="file" />
+          <button type="submit"> Sign up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -7,55 +7,57 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
-  try {
-    const displayName = req.body.displayName;
-    const email = req.body.email;
-    const account = req.body.account;
-    const isAdmin = req.body.isAdmin;
-    const password = req.body.password;
-    const file = req.body.file;
+  console.log(1)
+  // try {
+  //   console.log(req.body)
+  //   const displayName = req.body.displayName;
+  //   const email = req.body.email;
+  //   const account = req.body.account;
+  //   const isAdmin = req.body.isAdmin;
+  //   const password = req.body.password;
+  //   const file = req.body.file;
     
-    // Create user
-    // const res = await createUserWithEmailAndPassword(
-    //   auth,
-    //   email,
-    //   password
-    // );
-    // Create a unique image name
-    const date = new Date().getTime();
-    const storageRef = ref(storage, `${account + date}`);
-    // console.log(file);
-    await uploadBytesResumable(storageRef, file).then(() => {
-      getDownloadURL(storageRef).then(async (downloadURL) => {
-        try {
-          //Update profile
-          await updateProfile(res.user, {
-            account,
-            photoURL: downloadURL,
-          });
-          //create user on firestore
-          // await setDoc(doc(db, "users", res.user.uid), {
-          //   uid: res.user.uid,
-          //   account,
-          //   displayName,
-          //   email,
-          //   isAdmin,
-          //   photoURL: downloadURL,
-          // });
+  //   // Create user
+  //   // const res = await createUserWithEmailAndPassword(
+  //   //   auth,
+  //   //   email,
+  //   //   password
+  //   // );
+  //   // Create a unique image name
+  //   const date = new Date().getTime();
+  //   const storageRef = ref(storage, `${account + date}`);
+  //   // console.log(file);
+  //   await uploadBytesResumable(storageRef, file).then(() => {
+  //     getDownloadURL(storageRef).then(async (downloadURL) => {
+  //       try {
+  //         //Update profile
+  //         await updateProfile(res.user, {
+  //           account,
+  //           photoURL: downloadURL,
+  //         });
+  //         //create user on firestore
+  //         // await setDoc(doc(db, "users", res.user.uid), {
+  //         //   uid: res.user.uid,
+  //         //   account,
+  //         //   displayName,
+  //         //   email,
+  //         //   isAdmin,
+  //         //   photoURL: downloadURL,
+  //         // });
 
-          // //create empty user chats on firestore
-          // await setDoc(doc(db, "userChats", res.user.uid), {});
-          // navigate("/");
-        } catch (error) {
-          console.log(error);
-        }
-      });
-    });
-    // res.status(200).send(res);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
+  //         // //create empty user chats on firestore
+  //         // await setDoc(doc(db, "userChats", res.user.uid), {});
+  //         // navigate("/");
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     });
+  //   });
+  //   // res.status(200).send(res);
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).send(error);
+  // }
 
 });
 
