06042c97bcea2f968155c35b6f41877e9642d31e
JJerrychan <164768186@qq.com>
2022-12-07 17:10:54 -0500

    imported eslint & prettier

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,25 @@ import User from "./components/Users/profilePage";
 // import Signout from "./components/Signout";
 import Header from "./components/Header";
 import Password from "./components/Users/resetPassword";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {useContext} from "react";
-import {AuthContext} from "./context/AuthContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-
-    const {currentUser} = useContext(AuthContext)
-    return (
-        <BrowserRouter>
-            <Header/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/login" element={<Login/>}/>
-                {/* <Route path="/signout" element={<Signout />} /> */}
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/user" element={<User/>}/>
-                <Route path="/user/password" element={<Password/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+  const { currentUser } = useContext(AuthContext);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/signout" element={<Signout />} /> */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/user/password" element={<Password />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
