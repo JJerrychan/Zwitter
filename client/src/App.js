4801552a7688ad396714e83ef164ac4f86ba2d83
JJerrychan <164768186@qq.com>
2022-12-07 00:41:18 -0500

    import mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
 import "./App.css";
-import Register from "./page/Register";
-import Login from "./page/Login";
-import Home from "./page/Home";
-import User from "./page/Users/profilePage";
-// import Signout from "./page/Signout";
-import Header from "./page/Header";
-import Password from "./page/Users/resetPassword";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import User from "./components/Users/profilePage";
+// import Signout from "./components/Signout";
+import Header from "./components/Header";
+import Password from "./components/Users/resetPassword";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {useContext} from "react";
+import {AuthContext} from "./context/AuthContext";
 
 function App() {
 
-  const {currentUser} = useContext(AuthContext)
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/signout" element={<Signout />} /> */}
-        <Route path="/register" element={<Register />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/user/password" element={<Password />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    const {currentUser} = useContext(AuthContext)
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                {/* <Route path="/signout" element={<Signout />} /> */}
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/user" element={<User/>}/>
+                <Route path="/user/password" element={<Password/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
