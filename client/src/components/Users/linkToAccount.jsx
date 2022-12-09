7a52cc21b120bdb80f5ac4741a59292c15b67300
RookieZxy <46013671+RookieZxy@users.noreply.github.com>
2022-12-08 19:22:32 -0500

    link to google account

--- client/src/components/Users/linkToAccount.jsx
+++ client/src/components/Users/linkToAccount.jsx
@@ -1,13 +1,34 @@
 import React, { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
-import { doc, updateDoc } from "firebase/firestore";
-import { db } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+// import { doc, updateDoc } from "firebase/firestore";
+// import { db } from "../../firebase";
+import { auth } from "../../firebase";
+import { linkWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const DisplayName = () => {
-  const { currentUser } = useContext(AuthContext);
-  const LinkToFacebook = async (e) => {
-    try {
+  const googleProvider = new GoogleAuthProvider();
+  const navigate = useNavigate();
 
+  const LinkToGoogleAccount = async (e) => {
+    try {
+      linkWithPopup(auth.currentUser, googleProvider)
+        .then((result) => {
+          // Accounts successfully linked.
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          if (credential) {
+            // Accounts successfully linked.
+            const user = result.user;
+            console.log("success");
+            // ...
+          } else {
+            console.log(2);
+          }
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          console.log(error);
+          // ...
+        });
     } catch (error) {
       console.log(error);
     }
@@ -15,7 +36,7 @@ const DisplayName = () => {
 
   return (
     <div>
-      <p onClick={LinkToFacebook}>Link to Facebook</p>
+      <button onClick={LinkToGoogleAccount}>Link to Google Account</button>
     </div>
   );
 };
