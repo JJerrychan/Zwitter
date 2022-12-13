955d7ad1f95d170eb23780aa3abdd983308ebee9
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2022-12-13 03:04:09 -0500

    chat room temp

--- client/src/components/messages/MessengerTest.jsx
+++ client/src/components/messages/MessengerTest.jsx
@@ -4,11 +4,13 @@ import Message from "./Message";
 import ChatOneline from "./ChatOnline";
 import React, { useContext, useEffect, useState } from 'react';
 import { Card, CardActionArea, Grid, Link, TextField, Typography } from "@mui/material";
+import { redirect } from "react-router-dom";
 // import { format } from "timeago.js";
 
 const Messenger = () => {
   // const [conversations, setConversations] = useState([]);
   const [conversationsRoom, setconversationsRoom] = useState({});
+  const [conversationsRoomPassword, setconversationsRoomPassword] = useState({});
   //const [allRoomsGroup, setAllRoomsGroup] = useContext(RoomsContext);
   return (
     <Grid >
@@ -16,18 +18,22 @@ const Messenger = () => {
                 <Card>
                     <h1>Create Chat Room:</h1>
                     <TextField id="roomName" label="roomName" variant="outlined" />
-                    {/*<TextField id="roomPassword" label="roomPassword" variant="outlined" />*/}
+                    <TextField id="roomPassword" label="roomPassword" variant="outlined" />
                     < br/>
                     < br/>
                     <form  className='form' id='add-Room'
                         onSubmit={(e) => {
                             e.preventDefault();
                             const chatRoomName = document.getElementById('roomName').value.trim();
-                            //const chatRoomPassword = document.getElementById('roomPassword').value.trim();
+                            const chatRoomPassword = document.getElementById('roomPassword').value.trim();
                             if (chatRoomName === "") {
-                                alert("chatRoomName should not be empty!");
+                                alert("Chat room name should not be empty!");
                                 return;
                             }
+                            if (chatRoomPassword === "") {
+                              alert("Chat room password should not be empty!");
+                              return;
+                          }
                             /*
                             if (chatRoomPassword === "") {
                                 alert("chatRoomPassword should not be empty!");
@@ -35,6 +41,7 @@ const Messenger = () => {
                             }
                             */
                             setconversationsRoom({ ...conversationsRoom, [chatRoomName]: [], /*[chatRoomPassword]:[]*/ });
+                            setconversationsRoomPassword({ ...conversationsRoomPassword, [chatRoomPassword]: []});
                         }}
                     >
                         <button className='button' type='submit'>
@@ -45,13 +52,50 @@ const Messenger = () => {
                 </Card>
                 < br/>
                 {
-                    Object.keys(conversationsRoom).map((roomName) => {
+                    Object.keys(conversationsRoomPassword,conversationsRoom).map((password,roomName) => {
                         return(
                             <Grid container key={roomName}>
                                 <Grid container>
                                 <Typography gutterBottom variant='h6' component='h2'>Chat room: {roomName} 
+                                  <TextField id="roomPassword2" label="roomPassword2" variant="outlined" />
+                                    <p>room password: {password}</p>
                                     < br/>
-                                    <Link className='showlink' href='http://localhost:3000/messages'>JoinRoom</Link>
+                                    <form className="joinChatRoomPasswordForm" id = "joinChatRoomPasswordForm"
+                                      onSubmit={(e) => {
+                                        e.preventDefault();
+                                        const joinRoomPassword = document.getElementById('roomPassword2').value.trim();
+                                        if (joinRoomPassword === "") {
+                                          alert("Chat room password should not be empty!");
+                                          return;
+                                        }
+                                        <p>password enter: {joinRoomPassword}</p>
+                                        if (joinRoomPassword !== password) {
+                                          alert(`${joinRoomPassword} Chat room password is wrong!`);
+                                          return;
+                                        }
+                                        /*
+                                        if (joinRoomPassword === password) {
+                                          
+                                        }
+                                        */
+                                      }}
+                                    >
+                                      {/*
+                                        <button className='button'  type='submit' >
+                                          <Link href ='http://localhost:3000/messages'>
+                                            Join
+                                          </Link>
+                                        </button>
+                                      */}
+                                        <button className='button'  type='submit' to ='http://localhost:3000/messages'>
+                                          Join
+                                        </button>
+                                    </form>
+                                    {/*
+                                    {
+                                      document.getElementById('roomPassword2').value.trim() === password?<Link className='showlink' href='http://localhost:3000/messages'>JoinRoom</Link>:<p>no</p>
+                                    }
+                                    {/*<Link className='showlink' href='http://localhost:3000/messages'>JoinRoom</Link>*/}
                                     < br/>
                                 </Typography>
                                 </Grid>
