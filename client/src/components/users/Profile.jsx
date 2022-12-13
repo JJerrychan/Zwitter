9698c13348c3dc874f6aaf06a2a8f11a827985e5
JJerrychan <164768186@qq.com>
2022-12-13 18:21:34 -0500

    modified Profile component

--- client/src/components/users/Profile.jsx
+++ client/src/components/users/Profile.jsx
@@ -1,56 +1,91 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
-import Password from "./resetPassword";
-import Name from "./resetName";
-import Photo from "./resetPhoto";
-import { Container } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  ButtonGroup,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Container,
+  IconButton,
+  Stack,
+  Tab,
+  Tabs,
+} from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
 
 const Profile = () => {
   const { currentUser } = useContext(AuthContext);
-  const [form1, setForm1] = useState(false);
-  const [form2, setForm2] = useState(false);
-  const [form3, setForm3] = useState(false);
-  const handleName = () => {
-    // console.log(currentUser);
-    setForm1(false);
-    setForm2(true);
-    setForm3(false);
+  const [value, setValue] = useState(0);
+  const handleTabChange = (event, newValue) => {
+    setValue(newValue);
   };
-  const handlePassword = () => {
-    setForm1(true);
-    setForm2(false);
-    setForm3(false);
-  };
-  const handleProfilephoto = () => {
-    setForm1(false);
-    setForm2(false);
-    setForm3(true);
-  };
+  // const [form1, setForm1] = useState(false);
+  // const [form2, setForm2] = useState(false);
+  // const [form3, setForm3] = useState(false);
+  // const handleName = () => {
+  //   // console.log(currentUser);
+  //   setForm1(false);
+  //   setForm2(true);
+  //   setForm3(false);
+  // };
+  // const handlePassword = () => {
+  //   setForm1(true);
+  //   setForm2(false);
+  //   setForm3(false);
+  // };
+  // const handleProfilephoto = () => {
+  //   setForm1(false);
+  //   setForm2(false);
+  //   setForm3(true);
+  // };
 
   return (
     <Container>
-      <div>
-        <p style={{ display: currentUser != null ? "none" : "block" }}>
-          Please login first
-        </p>
-        <div style={{ display: currentUser == null ? "none" : "block" }}>
-          <p>Profile</p>
-          <div>
-            <button onClick={handlePassword}>reset password</button>
-            <button onClick={handleName}>change name</button>
-            <button onClick={handleProfilephoto}>Change Profile photo</button>
-            <div style={{ display: form1 ? "block" : "none" }}>
-              <Password />
-            </div>
-            <div style={{ display: form2 ? "block" : "none" }}>
-              <Name />
-            </div>
-            <div style={{ display: form3 ? "block" : "none" }}>
-              <Photo />
-            </div>
-          </div>
-        </div>
-      </div>
+      {currentUser ? (
+        <Card>
+          <CardHeader
+            avatar={
+              <IconButton size={"small"} sx={{ marginRight: "2rem" }}>
+                <ArrowBack fontSize="large" />
+              </IconButton>
+            }
+            title={currentUser.displayName}
+            titleTypographyProps={{ fontSize: "1.2rem", fontWeight: "bold" }}
+            subheader={`${currentUser.numZwitter} Posts`}
+          />
+          <CardContent>
+            <Stack alignItems={"center"} spacing={2}>
+              <Avatar
+                sx={{ width: 120, height: 120 }}
+                alt={currentUser.displayName}
+                src={currentUser.photoURL}
+              />
+              <ButtonGroup color={"info"} disableElevation variant={"outlined"}>
+                <Button>Change Name</Button>
+                <Button>Update Photo</Button>
+                <Button>Reset Password</Button>
+              </ButtonGroup>
+            </Stack>
+          </CardContent>
+          <CardActions>
+            <Tabs
+              sx={{ marginX: "auto" }}
+              value={value}
+              onChange={handleTabChange}
+              centered
+            >
+              <Tab label="My Posts" />
+              <Tab label="Item Two" />
+              <Tab label="Item Three" />
+            </Tabs>
+          </CardActions>
+        </Card>
+      ) : (
+        <>Please login first!</>
+      )}
     </Container>
   );
 };
