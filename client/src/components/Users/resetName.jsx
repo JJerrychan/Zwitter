63e5b8e0e7ccd096eb8054006123c3cd740b82f0
RookieZxy <46013671+RookieZxy@users.noreply.github.com>
2022-12-13 12:27:02 -0500

    upload resetName and resetPhoto

--- client/src/components/Users/resetName.jsx
+++ client/src/components/Users/resetName.jsx
@@ -1,10 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
+import { updateProfile } from "firebase/auth";
+
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
 
 const DisplayName = () => {
   const { currentUser } = useContext(AuthContext);
+  const [errorDialog, setErrorDialog] = useState(false);
+  const [error, setError] = useState();
+  const [successDialog, setSuccessDialog] = useState(false);
   const handleSubmit = async (e) => {
     try {
       const displayName = e.target[0].value;
@@ -13,17 +26,60 @@ const DisplayName = () => {
       }
       const washingtonRef = doc(db, "users", `${currentUser.uid}`);
 
+      await updateProfile(currentUser, {
+        displayName,
+        photoURL: currentUser.photoURL,
+      });
       // Set the "capital" field of the city 'DC'
       await updateDoc(washingtonRef, {
         displayName: displayName,
-      });
+      }).then(setSuccessDialog(true));
     } catch (error) {
-      console.log(error);
+      if (error.code != null) error = error.code;
+
+      setError(error);
+      setErrorDialog(true);
     }
   };
 
   return (
     <div>
+      <Dialog open={errorDialog} onClose={() => setErrorDialog(false)}>
+        <Box maxWidth={400}>
+          <DialogContent sx={{ display: "flex", justifyContent: "center" }}>
+            <DialogContentText
+              fontSize="large"
+              letterSpacing=".1rem"
+              fontWeight="bold"
+            >
+              {error}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions sx={{ justifyContent: "center" }}>
+            <Button color="warning" onClick={() => setErrorDialog(false)}>
+              Confirm
+            </Button>
+          </DialogActions>
+        </Box>
+      </Dialog>
+      <Dialog open={successDialog} onClose={() => setSuccessDialog(false)}>
+        <Box maxWidth={400}>
+          <DialogContent sx={{ display: "flex", justifyContent: "center" }}>
+            <DialogContentText
+              fontSize="large"
+              letterSpacing=".1rem"
+              fontWeight="bold"
+            >
+              Success!
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions sx={{ justifyContent: "center" }}>
+            <Button color="warning" onClick={() => setSuccessDialog(false)}>
+              Confirm
+            </Button>
+          </DialogActions>
+        </Box>
+      </Dialog>
       <p>change displayName</p>
       <form target="iFrame" onSubmit={handleSubmit}>
         <input required type="text" placeholder="new Name"></input>
--- client/src/components/Users/resetPhoto.jsx
+++ client/src/components/Users/resetPhoto.jsx
@@ -3,11 +3,21 @@ import { AuthContext } from "../../context/AuthContext";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { db, storage } from "../../firebase";
 import { doc, updateDoc } from "firebase/firestore";
+import { updateProfile } from "firebase/auth";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
 
 const DisplayName = () => {
   const { currentUser } = useContext(AuthContext);
   const [errorDialog, setErrorDialog] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState();
+  const [successDialog, setSuccessDialog] = useState(false);
   const handleSubmit = async (e) => {
     const file = e.target[0].files[0];
 
@@ -18,19 +28,64 @@ const DisplayName = () => {
 
       await uploadBytesResumable(storageRef, file).then(() => {
         getDownloadURL(storageRef).then(async (downloadURL) => {
+          const displayName = currentUser.displayName;
+          await updateProfile(currentUser, {
+            displayName,
+            photoURL: downloadURL,
+          });
           const userRef = doc(db, "users", `${currentUser.uid}`);
           await updateDoc(userRef, {
             photoURL: downloadURL,
           });
-        });
+        }).then(setSuccessDialog(true));
       });
     } catch (error) {
-      console.log(error.code);
+      if (error.code != null) error = error.code;
+
+      setError(error);
+      setErrorDialog(true);
     }
   };
 
   return (
     <div>
+      <Dialog open={errorDialog} onClose={() => setErrorDialog(false)}>
+        <Box maxWidth={400}>
+          <DialogContent sx={{ display: "flex", justifyContent: "center" }}>
+            <DialogContentText
+              fontSize="large"
+              letterSpacing=".1rem"
+              fontWeight="bold"
+            >
+              {error}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions sx={{ justifyContent: "center" }}>
+            <Button color="warning" onClick={() => setErrorDialog(false)}>
+              Confirm
+            </Button>
+          </DialogActions>
+        </Box>
+      </Dialog>
+      <Dialog open={successDialog} onClose={() => setSuccessDialog(false)}>
+        <Box maxWidth={400}>
+          <DialogContent sx={{ display: "flex", justifyContent: "center" }}>
+            <DialogContentText
+              fontSize="large"
+              letterSpacing=".1rem"
+              fontWeight="bold"
+            >
+              Success!
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions sx={{ justifyContent: "center" }}>
+            <Button color="warning" onClick={() => setSuccessDialog(false)}>
+              Confirm
+            </Button>
+          </DialogActions>
+        </Box>
+      </Dialog>
+
       <p>change profile photo</p>
       <form target="iFrame" onSubmit={handleSubmit}>
         <input required type="file"></input>
