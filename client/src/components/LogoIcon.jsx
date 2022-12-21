073940e817aa67a5f6a2ffecd19d92b383d41946
JJerrychan <164768186@qq.com>
2022-12-20 20:40:51 -0500

    README.MD

--- client/src/components/LogoIcon.jsx
+++
@@ -1,7 +0,0 @@
-import { ReactComponent as Logo } from "../public/zwitter-logo/svg/logo-primary.svg";
-import React from "react";
-import { SvgIcon } from "@mui/material";
-
-export default function LogoIcon(props) {
-  return <SvgIcon {...props} component={Logo} />;
-}
